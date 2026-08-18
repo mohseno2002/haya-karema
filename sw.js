62c083689c91ef443223bbff26f62143f37f77c6
mohseno2002 <mohseno2002@gmail.com>
2026-08-18 21:19:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker — لوحة استلامات دار الهندسة
 // يتيح تثبيت التطبيق والعمل بلا إنترنت (عدا التزامن السحابي)
-const CACHE='deh-v30';
+const CACHE='deh-v33';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
